81ecf8b92e2be227666cc931dbc937a16dc7480e
ka <ka.kaosf@gmail.com>
2013-08-17 14:45:44 +0900

    implement the validation for arguments

--- rename.sh
+++ rename.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+if [ $# -lt 3 ]; then
+  echo '3 arguments are required.' 2>&1
+  echo 'They are used for $1.$2.$3.' 2>&1
+  exit 1
+fi
+
 APP_ID="$1.$2.$3"
 
 sed -e "s/net.kaosfield.app/$APP_ID/" -i src/net/kaosfield/app/MainActivity.java
