cdc11357419890d5bd0c20f505c227868180df10
ka <ka.kaosf@gmail.com>
2013-09-02 17:35:07 +0900

    make not to modify res/values/strings.xml

--- rename.sh
+++ rename.sh
@@ -10,7 +10,6 @@ APP_ID="$1.$2.$3"
 
 sed -e "s/com.example.app/$APP_ID/" -i src/com/example/app/MainActivity.java
 sed -e "s/com.example.app/$APP_ID/" -i AndroidManifest.xml
-sed -e "s/com.example.app/$APP_ID/" -i res/values/strings.xml
 
 if [ ! -d src/com/example/$3 ]; then mv src/com/example/app src/com/example/$3; fi
 if [ ! -d src/com/$2 ];         then mv src/com/example     src/com/$2;         fi
